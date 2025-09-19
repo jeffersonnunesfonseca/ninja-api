0d991a480cf3971166ce308e5baaed879ece59fc
Jefferson Nunes Fonseca <jefferson.fonseca@vrsoft.com.br>
2025-09-19 08:31:56 -0300

    feat: implementação PUT + DELETE

--- tracks/api.py
+++ tracks/api.py
@@ -6,7 +6,7 @@ from django.http import HttpRequest
 from ninja import NinjaAPI
 
 from tracks.models import Track
-from tracks.schema import TrackSchema, NotFoundSchema
+from tracks.schema import TrackSchema, TrackSchemaResponse, NotFoundSchema
 
 api = NinjaAPI()
 
@@ -15,20 +15,42 @@ def health(request: HttpRequest):
     return "OK"
 
 
-@api.get('/tracks', response=List[TrackSchema])
+@api.get('/tracks', response=List[TrackSchemaResponse])
 def tracks(request: HttpRequest, title: Optional[str] = None):
     if title:
         return Track.objects.filter(title__icontains=title)
     return Track.objects.all()
 
-@api.get('/tracks/{track_id}', response={HTTPStatus.OK: TrackSchema, HTTPStatus.NOT_FOUND: NotFoundSchema})
+@api.get('/tracks/{track_id}', response={HTTPStatus.OK: TrackSchemaResponse, HTTPStatus.NOT_FOUND: NotFoundSchema})
 def tracks_by_id(request: HttpRequest, track_id: int):
-    # return Track.objects.all()
     try:
         return Track.objects.get(pk=track_id)
     except Track.DoesNotExist as e:
         return HTTPStatus.NOT_FOUND, {"message": "Track does not exists"}
 
-@api.post('/tracks', response={HTTPStatus.CREATED: TrackSchema})
+@api.post('/tracks', response={HTTPStatus.CREATED: TrackSchemaResponse})
 def create_track(request: HttpRequest, track: TrackSchema):
     return Track.objects.create(**track.dict())
+
+
+@api.put('/tracks/{track_id}', response={HTTPStatus.OK: TrackSchemaResponse, HTTPStatus.NOT_FOUND: NotFoundSchema})
+def update_track(request: HttpRequest, track_id: int, data: TrackSchema):
+    try:
+        track =  Track.objects.get(pk=track_id)
+        for attr, value in data.dict().items():
+            setattr(track, attr, value)
+        
+        track.save()
+        return HTTPStatus.OK, track
+    except Track.DoesNotExist as e:
+        return HTTPStatus.NOT_FOUND, {"message": "Track does not exists"}
+    
+
+@api.delete('/tracks/{track_id}', response={HTTPStatus.OK: None, HTTPStatus.NOT_FOUND: NotFoundSchema})
+def delete_track(request: HttpRequest, track_id: int):
+    try:
+        track =  Track.objects.get(pk=track_id)
+        track.delete()
+        return HTTPStatus.OK
+    except Track.DoesNotExist as e:
+        return HTTPStatus.NOT_FOUND, {"message": "Track does not exists"}
--- tracks/schema.py
+++ tracks/schema.py
@@ -1,11 +1,15 @@
+from typing import Optional
 from datetime import datetime
 from ninja import Schema
 
-class TrackSchema(Schema):
+class TrackSchema(Schema):    
     title: str
     artist: str
     duration: float
     last_play: datetime
+
+class TrackSchemaResponse(TrackSchema):
+    id: int
 
 class NotFoundSchema(Schema):
     message: str
