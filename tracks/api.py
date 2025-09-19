6efbf140e05639e8fd71e7f9d16c07a48845fe87
Jefferson Nunes Fonseca <jefferson.fonseca@vrsoft.com.br>
2025-09-19 17:14:21 -0300

    improvement: decorator que mantém o padrão de retorno

--- tracks/api.py
+++ tracks/api.py
@@ -1,36 +1,19 @@
 from http import HTTPStatus
 from typing import List, Optional
 
-import orjson
 from django.http import HttpRequest
 from ninja import NinjaAPI
-from ninja.renderers import BaseRenderer
 
 from tracks.models import Track
-from tracks.schema import BaseResponse, NotFoundSchema, TrackSchema, TrackSchemaResponse
+from tracks.schema import (
+    BaseResponse,
+    NotFoundSchema,
+    TrackSchema,
+    TrackSchemaResponse,
+    base_response,
+)
 
-from .schema import BaseResponseStatus
-
-
-class CustomRenderer(BaseRenderer):
-    media_type = "application/json"
-
-    def render(self, request: HttpRequest, data, *, response_status: int):
-        # Monta o formato padrão de resposta
-        body = {
-            "status": (
-                BaseResponseStatus.SUCCESS.value
-                if response_status < 400
-                else BaseResponseStatus.ERROR.value
-            ),
-            "message": "OK" if response_status < 400 else "Erro na requisição",
-            "data": data if response_status < 400 else None,
-            "error": data if response_status >= 400 else None,
-        }
-        return orjson.dumps(body)
-
-
-api = NinjaAPI(renderer=CustomRenderer())
+api = NinjaAPI()
 
 
 @api.get("/_health")
@@ -39,52 +22,55 @@ def health(request: HttpRequest):
 
 
 @api.get("/tracks", response=BaseResponse[List[TrackSchemaResponse]])
+@base_response
 def tracks(request: HttpRequest, title: Optional[str] = None):
     if title:
         return Track.objects.filter(title__icontains=title)
-    return Track.objects.all()
+    else:
+        return Track.objects.all()
 
 
 @api.get(
     "/tracks/{track_id}",
-    response={HTTPStatus.OK: TrackSchemaResponse, HTTPStatus.NOT_FOUND: NotFoundSchema},
+    response={
+        HTTPStatus.OK: BaseResponse[TrackSchemaResponse],
+        HTTPStatus.NOT_FOUND: BaseResponse[NotFoundSchema],
+    },
 )
+@base_response
 def tracks_by_id(request: HttpRequest, track_id: int):
-    try:
-        return Track.objects.get(pk=track_id)
-    except Track.DoesNotExist:
-        return HTTPStatus.NOT_FOUND, {"message": "Track does not exists"}
+    return Track.objects.get(pk=track_id)
 
 
-@api.post("/tracks", response={HTTPStatus.CREATED: TrackSchemaResponse})
+@api.post("/tracks", response={HTTPStatus.CREATED: BaseResponse[TrackSchemaResponse]})
+@base_response
 def create_track(request: HttpRequest, track: TrackSchema):
-    return Track.objects.create(**track.dict())
+    return HTTPStatus.CREATED, Track.objects.create(**track.dict())
 
 
 @api.put(
     "/tracks/{track_id}",
-    response={HTTPStatus.OK: TrackSchemaResponse, HTTPStatus.NOT_FOUND: NotFoundSchema},
+    response={
+        HTTPStatus.OK: BaseResponse[TrackSchemaResponse],
+        HTTPStatus.NOT_FOUND: BaseResponse[NotFoundSchema],
+    },
 )
+@base_response
 def update_track(request: HttpRequest, track_id: int, data: TrackSchema):
-    try:
-        track = Track.objects.get(pk=track_id)
-        for attr, value in data.dict().items():
-            setattr(track, attr, value)
+    track = Track.objects.get(pk=track_id)
+    for attr, value in data.dict().items():
+        setattr(track, attr, value)
 
-        track.save()
-        return HTTPStatus.OK, track
-    except Track.DoesNotExist:
-        return HTTPStatus.NOT_FOUND, {"message": "Track does not exists"}
+    track.save()
+    return track
 
 
 @api.delete(
     "/tracks/{track_id}",
-    response={HTTPStatus.OK: None, HTTPStatus.NOT_FOUND: NotFoundSchema},
+    response={HTTPStatus.OK: None, HTTPStatus.NOT_FOUND: BaseResponse[NotFoundSchema]},
 )
+@base_response
 def delete_track(request: HttpRequest, track_id: int):
-    try:
-        track = Track.objects.get(pk=track_id)
-        track.delete()
-        return HTTPStatus.OK
-    except Track.DoesNotExist:
-        return HTTPStatus.NOT_FOUND, {"message": "Track does not exists"}
+    track = Track.objects.get(pk=track_id)
+    track.delete()
+    return HTTPStatus.OK
--- tracks/schema.py
+++ tracks/schema.py
@@ -1,23 +1,25 @@
 from datetime import datetime
 from enum import Enum
+from functools import wraps
+from http import HTTPStatus
 from typing import Generic, Optional, TypeVar
 
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from ninja import Schema
+from ninja.errors import ValidationError
 
 T = TypeVar("T")
 
 
-class BaseResponseStatus(Enum):
+class BaseResponseStatus(str, Enum):
     ERROR = "error"
     SUCCESS = "success"
 
 
-# ninja suporta generics type
 class BaseResponse(Schema, Generic[T]):
     status: BaseResponseStatus
     message: str
     data: Optional[T] = None
-    error: Optional[dict] = None
 
 
 class TrackSchema(Schema):
@@ -33,3 +35,60 @@ class TrackSchemaResponse(TrackSchema):
 
 class NotFoundSchema(Schema):
     message: str
+
+
+HTTP_MAP = {
+    ObjectDoesNotExist: HTTPStatus.NOT_FOUND,
+    PermissionDenied: HTTPStatus.FORBIDDEN,
+    ValidationError: HTTPStatus.UNPROCESSABLE_ENTITY,
+}
+
+
+def base_response(func):
+    """
+    Mantem o padrão de retorno  'BaseResponse' de forma que não afeta o openapi
+    O BaseRender e Middleware não dão certo pois o pydantic força a validação
+    """
+
+    @wraps(func)
+    def wrapper(request, *args, **kwargs):
+        try:
+            result = func(request, *args, **kwargs)
+
+            status_code, message, data = HTTPStatus.OK, "OK", result
+
+            # Se o endpoint já retornou (status, message, data), respeita isso
+            if isinstance(result, tuple) and len(result) == 3:
+                status_code, message, data = result
+            elif isinstance(result, tuple) and len(result) == 2:
+                # Caso só (status, data)
+                status_code, data = result
+                message = HTTPStatus(status_code).phrase
+
+            response = BaseResponse(
+                status=BaseResponseStatus.SUCCESS,
+                message=message,
+                data=data if status_code != HTTPStatus.NO_CONTENT else None,
+            )
+            return status_code, response.dict()
+
+        except Exception as e:
+            # Verifica se a exceção está no mapa
+            for exc_type, http_code in HTTP_MAP.items():
+                if isinstance(e, exc_type):  # pega subclasses (ex: Track.DoesNotExist)
+                    response = BaseResponse(
+                        status=BaseResponseStatus.ERROR,
+                        message=str(e) or http_code.phrase,
+                        data=None,
+                    )
+                    return http_code, response.dict()
+
+            # fallback erro 500
+            response = BaseResponse(
+                status=BaseResponseStatus.ERROR,
+                message=str(e) or "Internal Server Error",
+                data=None,
+            )
+            return HTTPStatus.INTERNAL_SERVER_ERROR, response.dict()
+
+    return wrapper
