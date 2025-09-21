1c27378928ad1111bbeff202e576744c02a24256
Jefferson Nunes Fonseca <jefferson.fonseca@vrsoft.com.br>
2025-09-21 20:11:17 -0300

    feat: corrigir rotas da API para incluir o prefixo 'api/'

--- core/urls.py
+++ core/urls.py
@@ -9,11 +9,11 @@ from cobreja_app.v001.templates.api import router as templates_router_v001
 
 api = NinjaAPI()
 
-api.add_router("v001/charges/", charges_router_v001)
-api.add_router("v001/templates/", templates_router_v001)
-api.add_router("v001/rules/", rules_router_v001)
-api.add_router("v001/rule_steps/", rule_steps_router_v001)
-api.add_router("v001/installments/", installments_router_v001)
+api.add_router("api/v001/charges/", charges_router_v001)
+api.add_router("api/v001/templates/", templates_router_v001)
+api.add_router("api/v001/rules/", rules_router_v001)
+api.add_router("api/v001/rule_steps/", rule_steps_router_v001)
+api.add_router("api/v001/installments/", installments_router_v001)
 
 
 urlpatterns = [
