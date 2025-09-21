67dc5c7b1b29f9fe7dfcc156f6dc8af74e17bedd
Jefferson Nunes Fonseca <jefferson.fonseca@vrsoft.com.br>
2025-09-21 19:37:02 -0300

    feat: adicionar cabeçalho X-TOKEN com enumeração de ambiente nos testes de saúde

--- cobreja_app/v001/rule_steps/tests/test_api.py
+++ cobreja_app/v001/rule_steps/tests/test_api.py
@@ -1,11 +1,16 @@
 import pytest
 from django.test import Client
 
+from cobreja_app.shareds.enum import EnvrionmentEnum
+
 
 @pytest.mark.django_db
 def test_health_endpoint():
     client = Client()
-    response = client.get("/cobreja/v001/rule_steps/_health")
+    response = client.get(
+        "/cobreja/v001/rule_steps/_health",
+        headers={"X-TOKEN": EnvrionmentEnum.DEVELOPMENT},
+    )
 
     assert "RULE_STEPS_OK_" in response.text
     assert response.status_code == 200
